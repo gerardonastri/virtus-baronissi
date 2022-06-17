31ec4f502cf013729ea196bbe94049c9ece87a28
gerardonastri <gerardonastri06@gmail.com>
2022-06-17 17:06:11 +0200

    better

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import '../styles/globals.css'
 import React from 'react'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProp} />
 }
 
 export default MyApp
