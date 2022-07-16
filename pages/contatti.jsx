6263de6c3ebe7e093c76d489c705b584a3744a2e
gerardonastri <gerardonastri06@gmail.com>
2022-07-16 20:32:05 +0200

    better

--- pages/contatti.jsx
+++ pages/contatti.jsx
@@ -43,7 +43,9 @@ export default function Contatti() {
         <Navbar />
         <div className={styles.wrapper}>
             <h1>Contattaci</h1>
+          <div className={styles.desc}>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem dolores enim recusandae illo beatae consequatur, incidunt, animi adipisci voluptates quam pariatur. Nostrum, quidem odit tenetur optio blanditiis rerum repellendus voluptas.</p>
+          </div>
             <div className={styles.info}>
                 <div className={styles.single}>
                     <MapIcon />
@@ -67,16 +69,22 @@ export default function Contatti() {
                     </div>
                 </div>
             </div>
-            <div className={styles.form}>
-                <h2>I TUOI DATI</h2>
-                <span className={styles.error}>{error && error}</span>
-                <div className={styles.formFlex}>
-                    <input type="text" name="name" id="name" placeholder='Nome' value={nome} onChange={(e) => setNome(e.target.value)} />
-                    <input type="text" name="phone" id="phone" placeholder='Telefono'  value={telefono} onChange={(e) => setTelefono(e.target.value)} />
+            <div className={styles.content}>
+                <div className={styles.map}>
+                    {/* <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12089.96588087445!2d14.7803993!3d40.751214!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xd54fb2b2bc89ee46!2sPalazzetto%20dello%20sport%20%22Nando%20Nobile%22%20(Palairno)!5e0!3m2!1sit!2sit!4v1657993173985!5m2!1sit!2sit" style={{border:"0"}} allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe> */}
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12089.96588087445!2d14.7803993!3d40.751214!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xd54fb2b2bc89ee46!2sPalazzetto%20dello%20sport%20%22Nando%20Nobile%22%20(Palairno)!5e0!3m2!1sit!2sit!4v1657993173985!5m2!1sit!2sit" width="600" height="450" frameborder="0" style={{border:0}} allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
                 </div>
-                <input type="email" name="email" id="email" placeholder='Email'  value={email} onChange={(e) => setEmail(e.target.value)} />
-                <textarea name="message"  id="" cols="30" rows="10" placeholder='Messaggio'  defaultValue={messaggio} onChange={(e) => setMessaggio(e.target.value)}></textarea>
-                <button onClick={handleSubmit} style={{cursor: 'pointer'}}>Invia messaggio</button>
+                <div className={styles.form}>
+                    <h2>I TUOI DATI</h2>
+                    <span className={styles.error}>{error && error}</span>
+                    <div className={styles.formFlex}>
+                        <input type="text" name="name" id="name" placeholder='Nome' value={nome} onChange={(e) => setNome(e.target.value)} />
+                        <input type="text" name="phone" id="phone" placeholder='Telefono'  value={telefono} onChange={(e) => setTelefono(e.target.value)} />
+                    </div>
+                    <input type="email" name="email" id="email" placeholder='Email'  value={email} onChange={(e) => setEmail(e.target.value)} />
+                    <textarea name="message"  id="" cols="30" rows="10" placeholder='Messaggio'  defaultValue={messaggio} onChange={(e) => setMessaggio(e.target.value)}></textarea>
+                    <button onClick={handleSubmit} style={{cursor: 'pointer'}}>Invia messaggio</button>
+                </div>
             </div>
         </div>
         <Footer />
