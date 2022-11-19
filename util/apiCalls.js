c8d083c81d7e1085754b3c252f7254d248d77c1e
gerardonastri <gerardonastri06@gmail.com>
2022-11-19 10:15:51 +0100

    api url

--- util/apiCalls.js
+++ util/apiCalls.js
@@ -1,8 +1,8 @@
 import axios from "axios"
 
-const  BASE_URL = "http://localhost:3000/api/";
+// const  BASE_URL = "http://localhost:3000/api/";
 // const BASE_URL = "https://virtus-baronissi.vercel.app/api/"
-// const BASE_URL = "https://www.virtusbaronissi.it/api/"
+const BASE_URL = "https://www.virtusbaronissi.it/api/"
 
 export const axiosReq = axios.create({
     baseURL:  BASE_URL
