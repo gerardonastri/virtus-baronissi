db1373be20930129f7df3c697294892b0f42fce1
gerardonastri <gerardonastri06@gmail.com>
2023-04-01 14:19:34 +0200

    better

--- components/Slider.jsx
+++ components/Slider.jsx
@@ -3,6 +3,8 @@ import styles from '../styles/Slider.module.css'
 import useEmblaCarousel from 'embla-carousel-react'
 import {HiOutlineArrowLeft} from 'react-icons/hi'
 import {HiOutlineArrowRight} from 'react-icons/hi'
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
 
 const Slider = () => {
     const [emblaRef, emblaApi] = useEmblaCarousel()
@@ -19,6 +21,22 @@ const Slider = () => {
     return (
         <div className={styles.container}>
             <div className={styles.wrapper}>
+                <h1>Staff</h1>
+                <Carousel showArrows={true} showThumbs={false} className={styles.responsiveSlide} showStatus={false}>
+                        <img className={styles.img} src="foto-staff-1.jpeg" alt="immagine del personale virtus baronissi" />
+                        <img className={styles.img} src="foto-staff-2.jpeg" alt="immagine del personale virtus baronissi" />
+                        <img className={styles.img} src="foto-staff-3.jpeg" alt="immagine del personale virtus baronissi" />
+                        <img className={styles.img} src="foto-staff-4.jpeg" alt="immagine del personale virtus baronissi" />
+                    
+                        <img className={styles.img} src="foto-staff-6.jpeg" alt="immagine del personale virtus baronissi" />
+                        <img className={styles.img} src="foto-staff-7.jpeg" alt="immagine del personale virtus baronissi" />
+                        <img className={styles.img} src="foto-staff-8.jpeg" alt="immagine del personale virtus baronissi" />
+                        <img className={styles.img} src="foto-staff-9.jpeg" alt="immagine del personale virtus baronissi" />
+                    
+                        <img className={styles.img}  src="foto-staff-10.jpeg" alt="immagine del personale virtus baronissi" />
+                        <img className={styles.img}  src="foto-staff-11.jpeg" alt="immagine del personale virtus baronissi" />
+                    
+                </Carousel>
                 <div className={styles.embla} ref={emblaRef}>
                     <div className={styles.embla__container}>
                         <div className={styles.embla__slide}>
